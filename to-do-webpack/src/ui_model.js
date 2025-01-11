522dca23f13f51acebc434250d5fde0286132d3a
Vishnu <92211782+vishnu-sk@users.noreply.github.com>
2025-01-11 19:51:43 +0530

    added ui model file with classes for task item and project item

--- to-do-webpack/src/ui_model.js
+++ to-do-webpack/src/ui_model.js
@@ -1,20 +1,55 @@
-import {compareAsc, format} from "date-fns";
+import { compareAsc, formatDistanceToNowStrict } from "date-fns";
 
-class Todo{
+export class Todo {
+  constructor(task, description, deadline, priority) {
+    this.task = task;
+    this.deadline = new Date(deadline + "T00:00:00");
+    this.priority = priority;
+    this.description = description;
+    this.checked = false;
+    this.currentProject = "inbox";
+    this.updateCurrentDateGroup();
+  }
 
-    constructor(task, deadline, priority){
-        this.task = task;
-        this.deadline = deadline;
-        this.priority = priority;
-        this.checked = false;
+  changeCheckStatus() {
+    this.checked = !this.checked;
+  }
+
+  changePriority(newPrio) {
+    this.priority = newPrio;
+  }
+
+  updateCurrentDateGroup() {
+    const date = new Date();
+    const today = new Date(
+      `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`
+    );
+    if (compareAsc(today, this.deadline) == 0) {
+      this.currentDateGroup = "today";
+    } else if (compareAsc(today, this.deadline) == 1) {
+      const dist = formatDistanceToNowStrict(this.deadline, {
+        unit: "day",
+      });
+      if (dist == 2) {
+        this.currentDateGroup = "upcoming";
+      }
+    } else {
+      this.currentDateGroup = "expired";
+    }
+  }
+}
+
+class Project{
+    constructor(){
+        this.todoList = [];
     }
 
-    changeCheckStatus(){
-        this.checked = !this.checked;
+    addTaskToProject(todo){
+        this.todoList.push(todo);
     }
 
-    changePriority(newPrio){
-        this.priority = newPrio;
+    removeTaskFromProject(todo){
+        const index = this.todoList.indexOf(todo);
+        this.todoList.splice(index, 1);
     }
-
 }
