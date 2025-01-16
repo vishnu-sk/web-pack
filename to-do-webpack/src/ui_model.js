749acae0d70049e17bcd79858c46fe8de25ae5f6
Vishnu <92211782+vishnu-sk@users.noreply.github.com>
2025-01-17 01:22:23 +0530

    added validation for deadline in constructor for localstorage and normal init. Updated how date is calculated, distance, and compare date methods in updateCurrentDateGroup

--- to-do-webpack/src/ui_model.js
+++ to-do-webpack/src/ui_model.js
@@ -1,18 +1,28 @@
-import { compareAsc, formatDistanceToNowStrict } from "date-fns";
+import {
+  compareAsc,
+  formatDistanceToNowStrict,
+  formatDistanceStrict,
+} from "date-fns";
 
 class Todo {
-  constructor(task, description, deadline, priority) {
+  constructor(task, description, deadline, priority, currentProject = "inbox") {
     this.task = task;
-    this.deadline = new Date(deadline + "T00:00:00");
+    this.deadline =
+      deadline.length > 10
+        ? new Date(deadline.slice(0, 10) + "T00:00:00.000Z")
+        : new Date(deadline + "T00:00:00.000Z");
     this.priority = priority;
     this.description = description;
     this.checked = false;
-    this.currentProject = "inbox";
+    this.currentProject = currentProject;
     this.updateCurrentDateGroup();
   }
 
-  changeCheckStatus() {
-    this.checked = !this.checked;
+  trueCheckStatus() {
+    this.checked = true;
+  }
+  falseCheckStatus() {
+    this.checked = false;
   }
 
   changePriority(newPrio) {
@@ -22,16 +32,24 @@ class Todo {
   updateCurrentDateGroup() {
     const date = new Date();
     const today = new Date(
-      `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`
+      `${date.getUTCFullYear()}-${(date.getUTCMonth() + 1)
+        .toString()
+        .padStart(2, "0")}-${(date.getUTCDate() + 1)
+        .toString()
+        .padStart(2, "0")}T00:00:00.000Z`
     );
-    if (compareAsc(today, this.deadline) == 0) {
+    // console.log(
+    //   `comp - ${compareAsc(this.deadline, today)} : ${today} : ${this.deadline}`
+    //);
+    if (compareAsc(this.deadline, today) == 0) {
       this.currentDateGroup = "today";
-    } else if (compareAsc(today, this.deadline) == 1) {
-      const dist = formatDistanceToNowStrict(this.deadline, {
-        unit: "day",
-      });
-      if (dist == 2) {
+    } else if (compareAsc(this.deadline, today) == 1) {
+      const dist = formatDistanceStrict(today, this.deadline, { unit: "day" });
+      //console.log(`distance: ${dist}`);
+      if (dist == "2 days") {
         this.currentDateGroup = "upcoming";
+      } else {
+        this.currentDateGroup = "future";
       }
     } else {
       this.currentDateGroup = "expired";
@@ -39,29 +57,29 @@ class Todo {
   }
 }
 
-class Project{
-    constructor(name, list = []){
-        this.name = name;
-        this.todoList = list;
-        this.isActive = false;
-    }
+class Project {
+  constructor(name, list = []) {
+    this.name = name;
+    this.todoList = list;
+    this.isActive = false;
+  }
 
-    addTaskToProject(todo){
-        this.todoList.push(todo);
-    }
+  addTaskToProject(todo) {
+    this.todoList.push(todo);
+  }
 
-    removeTaskFromProject(todo){
-        const index = this.todoList.indexOf(todo);
-        this.todoList.splice(index, 1);
-    }
+  removeTaskFromProject(todo) {
+    const index = this.todoList.findIndex(i => i.task === todo.task);
+    this.todoList.splice(index, 1);
+  }
 
-    makeProjectActive(){
-      this.isActive = true;
-    }
+  makeProjectActive() {
+    this.isActive = true;
+  }
 
-    deactivateProject(){
-      this.isActive = false;
-    }
+  deactivateProject() {
+    this.isActive = false;
+  }
 }
 
-export {Todo, Project};
+export { Todo, Project };
